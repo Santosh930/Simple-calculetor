ff840af49c0372dcd1d7d3c6adf1645e52f14913
Santosh <santoshtajpur@gmail.com>
2023-08-18 13:32:48 +0530

    Calculetor app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import  {useRef,useState} from 'react';
 
 function App() {
+  const inputRef=useRef(null);
+  const resultRef=useRef(null);
+  const [result,setResult]=useState(0);
+  const plus=(e)=>{
+    e.preventDefault();
+    setResult((result)=>result+Number(inputRef.current.value));
+  }
+  const minus=(e)=>{
+    e.preventDefault();
+    setResult((result)=>result-Number(inputRef.current.value));
+  }
+  const times=(e)=>{
+    e.preventDefault();
+    setResult((result)=>result*Number(inputRef.current.value));
+  }
+  const divide=(e)=>{
+    e.preventDefault();
+    setResult((result)=>result/Number(inputRef.current.value));
+  }
+  const resetInput=(e)=>{
+    e.preventDefault();
+    inputRef.current.value=0;
+  }
+  const resetResult=(e)=>{
+    e.preventDefault();
+    setResult((preval)=>preval*0);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simplest Working Calculetor App</h1>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input pattern='[0-9]' ref={inputRef} type='number' placeholder='Enter a number!'/><br/><br/>
+        <button onClick={plus}>Add</button>
+        <button onClick={minus}>Substraction</button>
+        <button onClick={times}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form>
+      
+      
     </div>
   );
 }
